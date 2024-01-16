26c4f9ca62f7d59d08ebe9bdd294f6a88e2117f9
Sam Adrian <samadriansabalo99@gmail.com>
2024-01-16 16:08:28 +0800

    Update __init__.py

--- Engine/__init__.py
+++ Engine/__init__.py
@@ -6,7 +6,18 @@ db = SQLAlchemy()
 socketio = SocketIO()
 
 def create_app():
+    """
+    Creates and configures an instance of the Flask application.
 
+    This function initializes the Flask application, sets up the configuration,
+    initializes the database and socketio, and registers the blueprints for the
+    index, candidate, and error views.
+
+    Returns:
+    --------
+        app: A Flask application instance.
+    """
+    
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "Use your own secret key"
     app.config['SESSION_TYPE'] = "filesystem"
